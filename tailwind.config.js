833d516fdb58f23f0bfb118522c773cdaf43d136
ArturoJCastilloZ <castillo.arturo93@hotmail.com>
2025-05-23 15:25:26 -0600

    feat: add values in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      // Aquí puedes agregar estilos personalizados para el selector .home
+      // Por ejemplo, usando la opción `addComponents` en un plugin
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.hero': {
+          '@apply absolute inset-0 md:left-80 left-24 md:opacity-50 opacity-20': {},
+        },
+        '.img-hero': {
+          '@apply object-cover w-full h-full': {}
+        },
+        '.img-shadow': {
+          '@apply absolute inset-0 bg-gradient-to-r from-black via-black/35 to-transparent': {}
+        }
+      });
+    },
+  ],
 }
 
